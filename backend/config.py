e16959340f3048af7b14d46e9ef71095b8f9690f
shk365 <shwetakaurav365@gmail.com>
2026-05-23 17:13:46 +0530

    change: token expire time

--- backend/config.py
+++ backend/config.py
@@ -9,5 +9,5 @@ class Config:
     SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR, "database.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = "this_is_a_very_secure_secret_key_12345"
-    JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=1)
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=5)
     
